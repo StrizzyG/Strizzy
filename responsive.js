0f63831f7ce23238b0d943852ac6219ee2fc2076
StrizzyG <144599124+StrizzyG@users.noreply.github.com>
2024-07-06 15:50:23 +0200

    Add files via upload

---
+++ responsive.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const dropdownButton = document.querySelector('.dropdown-button');
+  const dropdownContent = document.querySelector('.dropdown-content');
+
+  dropdownButton.addEventListener('click', function() {
+    dropdownContent.classList.toggle('open');
+  });
+});
