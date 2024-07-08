f75e4a51b478df90306ba86e20f24ea2fe12d8fd
StrizzyG <144599124+StrizzyG@users.noreply.github.com>
2024-07-08 05:18:44 +0200

    Delete responsive.js

--- responsive.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-  const dropdownButton = document.querySelector('.dropdown-button');
-  const dropdownContent = document.querySelector('.dropdown-content');
-
-  dropdownButton.addEventListener('click', function() {
-    dropdownContent.classList.toggle('open');
-  });
-});
